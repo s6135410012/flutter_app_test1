ce8330161210164b2864f4c58046886671eb69ce
s6135410012 <s6135410012@sau.ac.th>
2021-08-25 17:09:00 +0700

    Code v.1.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app_test1/views/first_ui.dart';
+import 'package:flutter_app_test1/views/second_ui.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FirstUI(),
+      home: SecondUI(),
     )
   );
 }
